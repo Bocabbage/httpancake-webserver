2f94694f203ac57676bf03c39763f63a15030c67
Bocabbage <378066505@qq.com>
2022-06-13 19:07:35 +0800

    Update logging module: finish formatTime() func.

--- src/logging/Logger.cpp
+++ src/logging/Logger.cpp
@@ -2,11 +2,25 @@
 #include "NonBlockingLog.hpp"
 #include <pthread.h>
 #include <memory>
+#include <sstream>
+#include <chrono>
+#include <iomanip>
+using time_point = std::chrono::system_clock::time_point;
 
 static pthread_once_t once_control_ = PTHREAD_ONCE_INIT;
 static std::unique_ptr<NonBlockingLog> nbLogger_;
 
 string Logger::logFileName_ = "./httpancake.log";
+
+string serializeTimePoint( const time_point& time, const string& format)
+{
+    std::time_t tt = std::chrono::system_clock::to_time_t(time);
+    std::tm tm = *std::gmtime(&tt); //GMT (UTC)
+    //std::tm tm = *std::localtime(&tt); //Locale time-zone, usually UTC by default.
+    std::stringstream ss;
+    ss << std::put_time( &tm, format.c_str() );
+    return ss.str();
+}
 
 void once_init()
 {
@@ -29,7 +43,8 @@ Logger::Impl::Impl(const char *fileName, int line):
 
 void Logger::Impl::formatTime()
 {
-
+    auto timePoint = std::chrono::system_clock::now();
+    stream_ << '[' <<  serializeTimePoint(timePoint, "UTC: %Y-%m-%d %H:%M:%S") << ']';
 }
 
 Logger::Logger(const char *fileName, int line):
